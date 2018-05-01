b3ad46c8ebcefb6ff0f807fe8fe69a415d01cb51
Mikael Messias <mikaelmessias@outlook.com>
2018-05-01 00:29:24 -0300

    Add include guard macro on sort.h
    Signed-off-by: Mikael Messias <mikaelmessias@outlook.com>

--- src/sort/sort.h
+++ src/sort/sort.h
@@ -6,8 +6,13 @@
  * Furthermore, please read the LICENSE file for more information.
  ***************************************************************************/
 
+#ifndef SORT_H
+#define SORT_H
+
 /* @brief Sort an array[0...N-1] with the bubble sort algorithm.
  * @param v[] The array to be sorted.
  * @param n The maximum array size.
  */
 void bubble_sort(int v[], int n);
+
+#endif /* SORT_H */
