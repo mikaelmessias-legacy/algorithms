f9c804ad256dcfe1e7d00f06854cdff5a03b751a
Mikael Messias <mikaelmessias@outlook.com>
2018-05-01 16:02:20 -0300

    Add utils.c file to store functions non-related with sort
    Signed-off-by: Mikael Messias <mikaelmessias@outlook.com>

--- src/sort/main.c
+++ src/sort/main.c
@@ -9,33 +9,17 @@
 #include <stdio.h>
 #include "sort.h"
 
-/* @brief Prints an entire array.
- * @param v[] The array to be printed.
- * @param n The maximum array size.
- */
-void printa(int v[], int n);
-
 int main() {
     int v[] = {8, 7, 2, 9, 1};
     int lenght = sizeof v / sizeof v[0];
 
-    printf("Original:\n");
-    printa(v, lenght);
+    printf("Original:\t");
+    vector_print(v, lenght);
 
     bubble_sort(v, lenght);
 
-    printf("\nBubble Sort:\n");
-    printa(v, lenght);
+    printf("Bubble Sort:\t");
+    vector_print(v, lenght);
 
     return 0;
-}
-
-void printa(int v[], int n) {
-    int i;
-
-    for(i = 0; i < n; i++) {
-        printf("%d ", v[i]);
-    }
-
-    printf("\n");
 }
