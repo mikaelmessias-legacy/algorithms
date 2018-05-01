a6c56db2969e41674fe0528b2219545534b66474
Mikael Messias <mikaelmessias@outlook.com>
2018-05-01 00:28:23 -0300

    Add license header in all files
    Signed-off-by: Mikael Messias <mikaelmessias@outlook.com>

--- src/sort/bubble.c
+++ src/sort/bubble.c
@@ -1,3 +1,11 @@
+/****************************************************************************
+ * Copyright 2018 Mikael Messias <mikaelmessias@outlook.com>
+ *
+ * You can do whatever you want with this code, as long as you include the
+ * original copyright and license notice in any copy of the software/source.
+ * Furthermore, please read the LICENSE file for more information.
+ ***************************************************************************/
+
 #include <stdio.h>
 #include "../sort.h"
 
--- src/sort/sort.h
+++ src/sort/sort.h
@@ -1,3 +1,11 @@
+/****************************************************************************
+ * Copyright 2018 Mikael Messias <mikaelmessias@outlook.com>
+ *
+ * You can do whatever you want with this code, as long as you include the
+ * original copyright and license notice in any copy of the software/source.
+ * Furthermore, please read the LICENSE file for more information.
+ ***************************************************************************/
+
 /* @brief Sort an array[0...N-1] with the bubble sort algorithm.
  * @param v[] The array to be sorted.
  * @param n The maximum array size.
